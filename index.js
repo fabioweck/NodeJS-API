c2934380fe50049ddf15db73cc7a0d78bc09312a
Fabio Weck <fabio.weck@gmail.com>
2023-09-09 15:29:10 -0600

    new deploy/minor changes

--- index.js
+++ index.js
@@ -17,24 +17,41 @@ const loadFile = ()=>{
   fs.readFile(filePath, function (err, data) {
     if (err) 
         throw err;
+    
+    //Parses as array of objects
     jsonFile = JSON.parse(data)
+
+    //Sorts all entries to be displayed in order, starting form date then time
+    jsonFile.sort((a, b) => {
+
+      if (a.date < b.date) return -1;
+      if (a.date > b.date) return 1;
+    
+      if (a.start < b.start) return -1;
+      if (a.start > b.start) return 1;
+    
+      return 0;
+      
+    });
+
   });
 }
 
+//Calls load file
 loadFile();
 
-
+//Middleware to avoid CORS policy and methods problems
 app.use((req, res, next) => {
 
   res.header(
-
       "Access-Control-Allow-Headers",
-
       "Origin, X-Requested-With, Content-Type, Accept"
   );
 
-  res.header('Access-Control-Allow-Origin', '*'); // Allow requests from any origin
-  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE'); // Specify the allowed HTTP methods
+  res.header('Access-Control-Allow-Origin', '*'); 
+
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+
   next();
 
 });
@@ -43,20 +60,21 @@ app.options('*', (req, res) => {
   res.status(200).end();
 });
 
+//Delivers the original json file
 app.get('/rooms', function (req, res) {
   res.send(JSON.stringify(jsonFile))
 })
 
+
+//Route to get json file
 app.get('/', function (req, res) {
-  res.send(jsonFile);
+  res.status(200).send(jsonFile);
 });
 
-app.listen(port, "", ()=>{
-  console.log(`Server running on port ${port}`)
-})
-
+//Route to insert a new entry to the json file
 app.post('/add_room', (req, res)=>{
 
+  //variable to define the id of the entry
   let idCounter = jsonFile.length;
 
   let response = { 
@@ -67,39 +85,78 @@ app.post('/add_room', (req, res)=>{
     end: req.body.end
   }
 
+  if(!response.id || !response.date || !response.room || !response.start || !response.end)
+  {
+    console.log("Invalid entry.")
+    res.status(400).send("Invalid entry - please check fields.");
+    return;
+  }
+
+  //Adds the entry to the json file
   jsonFile.push(response);
+
+  //Sorts the entries based first on date and then time, to be displayed in order
+  jsonFile.sort((a, b) => {
+
+    if (a.date < b.date) return -1;
+    if (a.date > b.date) return 1;
   
-  fs.writeFile(filePath, JSON.stringify(jsonFile), ()=>{
-    console.log(`Entry ID:${response.id} added.`);
+    if (a.start < b.start) return -1;
+    if (a.start > b.start) return 1;
+  
+    return 0;
+
+  });
+  
+  //Writes the json file
+  fs.writeFile(filePath, JSON.stringify(jsonFile), (err)=>{
+    if(err){
+      console.log(err);
+      res.status(500).send("Error writing to file");
+    }
+    else{
+      console.log(`File written - Entry ${response.id} added.`);
+      res.status(200).send(`Entry ${response.id} added.`);
+    };  
   });
 
-  res.send("File written.");
+  res.send(`Entry ${response.id} added.`);
+
 });
 
+//Route to delete an entry
 app.delete('/delete_room', (req, res)=>{
 
+  //Gets the id of the item to be removed
   let id = req.body.ident;
 
+  //replies invalid in case of not receiving the id
   if (typeof id !== 'number') {
     return res.status(400).send('Invalid ID format');
   }
 
+  //filters the whole file removing only the item with the specific id
   let newList = jsonFile.filter((item)=>{
      if(item.id !== id) return item
   });
 
+
+  //writes the json file
   fs.writeFile(filePath, JSON.stringify(newList), (err)=>{
     if(err){
       console.log(err);
       res.status(500).send("Error writing to file");
     }
     else{
-      console.log(newList);
-      console.log(`Entry ${id} deleted.`);
-      loadFile();
+      console.log(`File written - Entry ${id} deleted.`);
       res.send(`Entry ${id} deleted.`);
     }
     
   });
 
+})
+
+//runs the server
+app.listen(port, "", ()=>{
+  console.log(`Server running on port ${port}`)
 })
